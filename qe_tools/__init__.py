5fe555c3aab02edb62e250e12d85d5ee1b463f06
Dominik Gresch <greschd@users.noreply.github.com>
2021-03-16 15:55:13 +0100

    Bump version to `2.0.0rc2`. (#60)

--- qe_tools/__init__.py
+++ qe_tools/__init__.py
@@ -6,6 +6,6 @@ from . import parsers
 from . import converters
 from . import exceptions
 
-__version__ = "2.0.0rc1"
+__version__ = "2.0.0rc2"
 
 __all__ = ('CONSTANTS', 'parsers', 'converters', 'exceptions')
