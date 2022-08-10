ba1e2d0e61fe5ffd2515d804bd8e36b925957da6
Sebastiaan Huber <mail@sphuber.net>
2022-08-10 11:32:55 +0200

    Remove `setup.py`, correct `pyproject.toml` and CD workflow

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-import json
-
-if __name__ == '__main__':
-    with open('setup.json', 'r') as info:
-        kwargs = json.load(info)
-    setup(include_package_data=True, packages=find_packages(), **kwargs)
