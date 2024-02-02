ce7279ca4e45025b2905b6329306fe52ceeebaff
Edan Bainglass <45081142+edan-bainglass@users.noreply.github.com>
2024-02-02 14:53:24 +0100

    (PR #74) Add missing import path for `extractors`

--- src/qe_tools/__init__.py
+++ src/qe_tools/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """A set of useful tools to manage Quantum ESPRESSO files."""
 from ._constants import DEFAULT as CONSTANTS  # isort:skip
-from . import converters, exceptions, parsers  # isort:skip
+from . import converters, exceptions, parsers, extractors  # isort:skip
 
 __version__ = '2.1.0'
 
-__all__ = ('CONSTANTS', 'parsers', 'converters', 'exceptions')
+__all__ = ('CONSTANTS', 'parsers', 'converters', 'exceptions', 'extractors')
