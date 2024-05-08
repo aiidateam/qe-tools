e85d76873675dd6f78ae41746f4364dff8b7680b
Michail Minotakis <minotakm@tcd.ie>
2024-05-08 09:09:42 +0200

    Update src/qe_tools/_constants.py
    Co-authored-by: Marnik Bercx <mbercx@gmail.com>

--- src/qe_tools/_constants.py
+++ src/qe_tools/_constants.py
@@ -23,7 +23,7 @@ DEFAULT = SimpleNamespace(
     timeau_to_sec=2.418884326155573e-17,
     invcm_to_THz=0.0299792458,
 
-    ## Values taken from https://gitlab.com/QEF/q-e/-/blob/develop/Modules/constants.f90?ref_type=heads#L66
+    ## Values taken from https://gitlab.com/QEF/q-e/-/blob/develop/Modules/constants.f90
     ha_SI=4.3597447222071e-18, # J
     bohr_SI=0.529177210903e-10, # m
     # From the definition of Quantum ESPRESSO, conversion from atomic mass
