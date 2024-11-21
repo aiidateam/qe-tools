a72eb3ff564815b447a3821e142057e414572f7c
Marnik Bercx <mbercx@gmail.com>
2024-11-21 10:59:38 +0100

    ✨🧪 Add missing `qes-1.0.xsd` schema and XML parsing tests
    The first schema used for the XML output of Quantum ESPRESSO `pw.x` was still missing.
    Here it is added, along with a set of tests for the XML parsing of default runs.

---
+++ tests/outputs/test_pw_output.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Tests for the ``PwOutput``."""
+
+import pytest
+
+from qe_tools.outputs.pw import PwOutput
+from importlib.resources import files
+from fixtures import pw
+
+
+@pytest.mark.parametrize(
+    'xml_format',
+    [
+        '190304',
+        '191206',
+        '200420',
+        '210716',
+        '211101',
+        '220603',
+        '230310',
+        '240411',
+    ],
+)
+def test_default_xml(data_regression, xml_format):
+    """Test the default XML output of pw.x."""
+
+    name = f'default_xml_{xml_format}'
+
+    directory = files(pw).joinpath(name)
+
+    pw_out = PwOutput.from_dir(directory)
+    pw_out.outputs
+
+    data_regression.check(
+        {
+            'outputs': pw_out.outputs,
+        }
+    )
