f68ac1a2a32d0137493578948a79ad6eafdb4ec5
Marnik Bercx <mbercx@gmail.com>
2024-11-21 10:26:41 +0100

    👌 `PwOutput`: Small changes to code
    Several small changes are made to the `PwOutput` class:
    
    1. Use `pathlib` instead of `os.path` for path definitions.
    2. `.from_dir` method: Only look for files from the specified directory using list
       comprehension.
    3. Adapt approach for identifying the `stdout` file to only use the first 5 lines to
       look for the `Program PWSCF` string.
    4. Remove the try-except loop that simply catches a general exception and raises a
       `ValueError` instead. This would obscure the actual error without an immediate
       benefit.
    5. Define the `d_out_xxx` variables as empty dictionaries before parsing, and redefine
       them in case the data is parsed successfully.
    6. Allow the user to pass a `Path` instead of a string to specify the directory to
       parse from.

--- src/qe_tools/outputs/pw.py
+++ src/qe_tools/outputs/pw.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """Output of the Quantum ESPRESSO pw.x code."""
 
-import os
+from typing import Union
+
+from pathlib import Path
 
 from qe_tools.outputs.base import BaseOutput
 from qe_tools.outputs.parsers.pw import PwStdoutParser, PwXMLParser
@@ -10,40 +12,39 @@ from qe_tools.outputs.parsers.pw import PwStdoutParser, PwXMLParser
 class PwOutput(BaseOutput):
     """Output of the Quantum ESPRESSO pw.x code."""
 
-    def __init(self, outputs: dict | None = None):
+    def __init__(self, outputs: dict | None = None):
         super().__init__(outputs=outputs, executable='pw.x')
 
     @classmethod
-    def from_dir(cls, directory: str, filetype: str = 'both'):
+    def from_dir(cls, directory: Union[str, Path], filetype: str = 'both'):
         """
         From a directory, locates the standard output and XML files and
         parses them.
         """
-        if not os.path.isdir(directory):
-            raise ValueError(f'Directory {directory} does not exist.')
+        directory = Path(directory)
 
-        parser_std = None
-        parser_xml = None
-        for file in os.listdir(directory):
-            if filetype in ['both', 'stdout'] and not os.path.isdir(file):
-                try:
-                    with open(file, 'r') as f:
-                        s = f.read()
-                    if 'Program PWSCF' in s:
-                        parser_std = PwStdoutParser(string=s)
+        if not directory.is_dir():
+            raise ValueError(f'Path `{directory}` is not a valid directory.')
+
+        d_out_std = {}
+        d_out_xml = {}
+
+        for file in [path for path in directory.iterdir() if path.is_file()]:
+            if filetype in ['both', 'stdout']:
+                with file.open('r') as handle:
+                    header = ''.join(handle.readlines(5))
+
+                    if 'Program PWSCF' in header:
+                        handle.seek(0)
+                        parser_std = PwStdoutParser(string=handle.read())
                         parser_std.parse()
-                except Exception:
-                    raise ValueError(f'The filename {file} leads to errors when parsing.')
-            if filetype in ['both', 'xml']:
-                try:
-                    if file.endswith('.xml'):
-                        parser_xml = PwXMLParser.from_file(filename=file)
-                        parser_xml.parse()
-                except Exception:
-                    raise ValueError(f'The filename {file} leads to errors when parsing.')
+                        d_out_std = parser_std.dict_out
 
-        d_out_std = {} if parser_std is None else parser_std.dict_out
-        d_out_xml = {} if parser_xml is None else parser_xml.dict_out
+            if filetype in ['both', 'xml']:
+                if file.suffix == '.xml':
+                    parser_xml = PwXMLParser.from_file(filename=file)
+                    parser_xml.parse()
+                    d_out_xml = parser_xml.dict_out
 
         outputs = d_out_std | d_out_xml
 
