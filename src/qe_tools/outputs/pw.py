40515c807c9e213157bc5e097daf0afd0fadc80d
Marnik Bercx <mbercx@gmail.com>
2025-10-04 07:22:45 +1000

    ✨ `PwOutput`: add `from_files` method
    Currently, the user can specify which file they want to parse from via the `filetype`
    input argument of the `from_dir` method. However, this is a bit unintuitive, and
    it's not immediately clear what the options are (they are also missing from the
    docstring). Morever, it doesn't fit various use cases, such as e.g. parsing input
    from a file handle.
    
    Here we remove the `filetype` input and instead add a `from_files` method which
    parses the outputs directly from the provided files with the corresponding parser class.
    The `from_dir` method is also adapted to use this method.
    
    We decided to add `*` to the function signature to force the user to be explicit in
    what files they want to import from with keyword arguments.

--- src/qe_tools/outputs/pw.py
+++ src/qe_tools/outputs/pw.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import TextIO
 
 from qe_tools.outputs.base import BaseOutput
 from qe_tools.outputs.parsers.pw import PwStdoutParser, PwXMLParser
@@ -15,7 +16,7 @@ class PwOutput(BaseOutput):
         super().__init__(outputs=outputs)
 
     @classmethod
-    def from_dir(cls, directory: str | Path, filetype: str = "both"):
+    def from_dir(cls, directory: str | Path):
         """
         From a directory, locates the standard output and XML files and
         parses them.
@@ -25,25 +26,39 @@ class PwOutput(BaseOutput):
         if not directory.is_dir():
             raise ValueError(f"Path `{directory}` is not a valid directory.")
 
-        d_out_std = {}
-        d_out_xml = {}
+        stdout_file = None
+        xml_file = None
 
         for file in [path for path in directory.iterdir() if path.is_file()]:
-            if filetype in ["both", "stdout"]:
-                with file.open("r") as handle:
-                    header = "".join(handle.readlines(5))
+            if file.suffix == ".xml":
+                xml_file = file
 
-                    if "Program PWSCF" in header:
-                        handle.seek(0)
-                        parser_std = PwStdoutParser(string=handle.read())
-                        parser_std.parse()
-                        d_out_std = parser_std.dict_out
+            with file.open("r") as handle:
+                header = "".join(handle.readlines(5))
 
-            if filetype in ["both", "xml"] and file.suffix == ".xml":
-                parser_xml = PwXMLParser.from_file(filename=file.as_posix())
-                parser_xml.parse()
-                d_out_xml = parser_xml.dict_out
+                if "Program PWSCF" in header:
+                    stdout_file = file
 
-        outputs = d_out_std | d_out_xml
+        return cls.from_files(xml=xml_file, stdout=stdout_file)
+
+    @classmethod
+    def from_files(
+        cls,
+        *,
+        xml: None | str | Path | TextIO = None,
+        stdout: None | str | Path | TextIO = None,
+    ):
+        """Parse the outputs directly from the provided files."""
+        outputs = {}
+
+        if stdout is not None:
+            parser_std = PwStdoutParser.from_file(stdout)
+            parser_std.parse()
+            outputs |= parser_std.dict_out
+
+        if xml is not None:
+            parser_xml = PwXMLParser.from_file(xml)
+            parser_xml.parse()
+            outputs |= parser_xml.dict_out
 
         return cls(outputs=outputs)
