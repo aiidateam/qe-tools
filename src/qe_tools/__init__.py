441a54fe6c469a71139863e6a2699cb4b8b6d88d
Marnik Bercx <mbercx@gmail.com>
2025-10-06 16:58:22 +1000

    🐛 `ELEMENTS`: make available from root import
    The `ELEMENTS` were actually imported in the `ase.py` module, but not
    available from the root of the package.

--- src/qe_tools/__init__.py
+++ src/qe_tools/__init__.py
@@ -1,6 +1,7 @@
 """Tools for running and parsing Quantum ESPRESSO calculations"""
 
 from ._constants import DEFAULT as CONSTANTS  # isort:skip -> Avoid circular imports
+from ._elements import ELEMENTS
 from . import converters, exceptions, extractors  # isort:skip
 
-__all__ = ("CONSTANTS", "converters", "exceptions", "extractors")
+__all__ = ("CONSTANTS", "ELEMENTS", "converters", "exceptions", "extractors")
